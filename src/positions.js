dab2dbdba09dbfd48bafd98221405772bc1987d0
Simone Gentili <sensorario@gmail.com>
2025-04-22 12:29:29 +0200

    feat(openings): add missing Queen's Pawn Opening

--- src/positions.js
+++ src/positions.js
@@ -8,16 +8,8 @@ const positions = [
         fen: "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R",
     },
     {
-        name: "Four Knights",
-        fen: "r1bqkb1r/pppppppp/2n2n2/8/8/2N2N2/PPPPPPPP/R1BQKB1R",
-    },
-    {
-        name: "Ruy Lopez",
-        fen: "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R",
-    },
-    {
-        name: "Central Game",
-        fen: "r1bqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR",
+        name: "Queen's Pawn Opening: Zukertort, Chigorin Variation",
+        fen: "r1bqkb1r/ppp1pppp/2n2n2/3p4/3P1B2/4PN2/PPP2PPP/RN1QKB1R",
     },
 ];
 
